07caaeb01169c8df33c42ee513edbe6abd2b5d2c
Nico Angelo <nicolas.angelo@gmail.com>
2021-12-02 17:52:01 +0100

    finalized day1

--- day1/README.md
+++ day1/README.md
@@ -4,6 +4,10 @@
 
 ```sh
 go mod init aoc2021/day1
+go run .
 ```
 
-## Documentation
+## References
+
+- Get started: https://go.dev/doc/tutorial/getting-started
+- `bufio.Scanner`: https://stackoverflow.com/questions/8757389/reading-a-file-line-by-line-in-go#16615559
--- day1/day1.go
+++ day1/day1.go
@@ -1,19 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
 	"log"
+	"os"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("Getting input data")
-
-	content, err := ioutil.ReadFile("./data")
+	file, err := os.Open("./data")
+	defer file.Close()
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	fmt.Println(string(content))
+	scanner := bufio.NewScanner(file)
+
+	previous_num := 0
+	increased_count := 0
+	first_run := true
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		num, _ := strconv.Atoi(line)
+		if first_run {
+			first_run = false
+		} else if num > previous_num {
+			increased_count++
+		}
+		previous_num = num
+	}
+
+	fmt.Println("Answer:", increased_count)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalln(err)
+	}
 }
