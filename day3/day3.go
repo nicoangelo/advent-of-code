b0fdbdf589f1966109223afbd28421cfdd8a93b7
Nico Angelo <nicolas.angelo@gmail.com>
2021-12-05 15:24:01 +0100

    day 3 part 2

--- day3/day3.go
+++ day3/day3.go
@@ -12,12 +12,12 @@ import (
 
 func main() {
 	input_path := flag.String("input", "./input", "The input data")
-	read_file(*input_path)
+	contents, line_count := get_file_contents(*input_path)
+	part1(contents, line_count)
+	part2(contents, line_count)
 }
 
-func read_file(filepath string) int {
-
-	contents, line_count := get_file_contents(filepath)
+func part1(contents [][]int, line_count int) int64 {
 	ones := count_ones(contents)
 	most_common_bits := get_most_common_bits(ones, line_count)
 	decimal, _ := strconv.ParseInt(most_common_bits, 2, 32)
@@ -28,10 +28,64 @@ func read_file(filepath string) int {
 	fmt.Println("gamma: ", decimal, ", epsilon:", inverted_decimal)
 	fmt.Println("power consumption:", result)
 
-	return int(result)
+	return result
 }
 
-func get_file_contents(filepath string) ([][]int, int) {
+func part2(contents [][]int, line_count int) int64 {
+	most_common_bits := find_column_value_by_criterion(contents, true)
+	decimal, _ := strconv.ParseInt(most_common_bits, 2, 32)
+	least_common_bits := find_column_value_by_criterion(contents, false)
+	inverted_decimal, _ := strconv.ParseInt(least_common_bits, 2, 32)
+	result := decimal * inverted_decimal
+
+	fmt.Println("oxygen generator rating: ", decimal, ", co2 scrubber rating:", inverted_decimal)
+	fmt.Println("life support rating:", result)
+
+	return result
+}
+
+func find_column_value_by_criterion(contents [][]int, most_common bool) string {
+	columns_count := len(contents[0])
+	result := ""
+	var selected_indices []int = nil
+
+	for i := 0; i < columns_count; i++ {
+		iteration_length := len(contents)
+		if selected_indices != nil {
+			iteration_length = len(selected_indices)
+		}
+		if iteration_length == 1 {
+			result += strconv.Itoa(contents[selected_indices[0]][i])
+			continue
+		}
+
+		one_indices := []int{}
+		zero_indices := []int{}
+		for j := 0; j < iteration_length; j++ {
+			current_index := j
+			if selected_indices != nil {
+				current_index = selected_indices[j]
+			}
+			if contents[current_index][i] == 0 {
+				zero_indices = append(zero_indices, current_index)
+			} else {
+				one_indices = append(one_indices, current_index)
+			}
+		}
+
+		if (most_common && len(one_indices) >= len(zero_indices)) ||
+			(!most_common && len(one_indices) < len(zero_indices)) {
+			selected_indices = one_indices
+			result += "1"
+		} else {
+			selected_indices = zero_indices
+			result += "0"
+		}
+	}
+	return result
+}
+
+func get_file_contents(filepath string) (contents [][]int, line_count int) {
 
 	file, err := os.Open(filepath)
 
@@ -40,9 +94,6 @@ func get_file_contents(filepath string) ([][]int, int) {
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-
-	var contents [][]int
-	line_count := 0
 
 	for scanner.Scan() {
 		line := scanner.Text()
--- day3/day3_test.go
+++ day3/day3_test.go
@@ -2,9 +2,21 @@ package main
 
 import "testing"
 
-func TestRead_file(t *testing.T) {
-	res := read_file("./example")
-	if res != 198 {
-		t.Fatalf("Epsilon wrong, got: %d, want: %d", res, 198)
+const power_consumption = 198
+const life_support_rating = 230
+
+func TestPart1(t *testing.T) {
+	contents, line_count := get_file_contents("./example")
+	res := part1(contents, line_count)
+	if res != power_consumption {
+		t.Fatalf("Epsilon wrong, got: %d, want: %d", res, power_consumption)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	contents, line_count := get_file_contents("./example")
+	res := part2(contents, line_count)
+	if res != life_support_rating {
+		t.Fatalf("Life support rating wrong, got: %d, want: %d", res, life_support_rating)
 	}
 }
