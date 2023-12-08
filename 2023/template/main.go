0a276bcf21d562e21fdbbacd1269fe4f572e7dc6
Nicolas Angelo <nicolas.angelo@gmail.com>
2023-12-08 18:47:10 +0100

    prepare for 2023

---
+++ 2023/template/main.go
@@ -0,0 +1,16 @@
+package dayX
+
+import (
+	"log"
+
+	"github.com/nicoangelo/aoc-pkg/reader"
+)
+
+func PrintSolutions() {
+	lines := reader.ReadInputFile("./dayX/input")
+	part1 := part1(lines)
+	log.Println("Part 1: ", part1)
+
+	part2 := part2(lines)
+	log.Println("Part 2: ", part2)
+}
---
+++ 2023/template/solutions.go
@@ -0,0 +1,9 @@
+package dayX
+
+func part1(lines []string) int {
+	return 0
+}
+
+func part2(lines []string) int {
+	return 0
+}
