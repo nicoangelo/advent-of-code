4d84d435eee31b3b2fe92da5d8044c6900ae889c
Nicolas Angelo <nicolas.angelo@gmail.com>
2023-12-22 18:33:02 +0100

    doc

--- 2023/day7/camelcards/handvaluator.go
+++ 2023/day7/camelcards/handvaluator.go
@@ -28,28 +28,28 @@ func (hv *ClassicHandValuator) getCardValue(c Card) int {
 	return defaultCardValues[c]
 }
 
-func (hv *ClassicHandValuator) getHandValue(cv map[Card]int) int {
+func (hv *ClassicHandValuator) getHandValue(cardCounts map[Card]int) int {
 	highestCount := 0
-	for _, v := range cv {
+	for _, v := range cardCounts {
 		if v > highestCount {
 			highestCount = v
 		}
 	}
-	if len(cv) == 1 {
+	if len(cardCounts) == 1 {
 		return FiveOfAKind
-	} else if len(cv) == 2 && highestCount == 4 {
+	} else if len(cardCounts) == 2 && highestCount == 4 {
 		// Four of a kind: 4+1
 		return FourOfAKind
-	} else if len(cv) == 2 {
+	} else if len(cardCounts) == 2 {
 		// Full House: 3+2
 		return FullHouse
-	} else if len(cv) == 3 && highestCount == 3 {
+	} else if len(cardCounts) == 3 && highestCount == 3 {
 		// Three of a kind: 3+1+1
 		return ThreeOfAKind
-	} else if len(cv) == 3 {
+	} else if len(cardCounts) == 3 {
 		// Two pair: 2+2+1
 		return TwoPair
-	} else if len(cv) == 4 {
+	} else if len(cardCounts) == 4 {
 		// One pair: 2+1+1+1
 		return OnePair
 	}
@@ -67,53 +67,59 @@ func (hv *JokerHandValuator) getCardValue(c Card) int {
 	return defaultCardValues[c]
 }
 
-func (hv *JokerHandValuator) getHandValue(cv map[Card]int) int {
+func (hv *JokerHandValuator) getHandValue(cardCounts map[Card]int) int {
 	highestCount := 0
-	for _, v := range cv {
+	for _, v := range cardCounts {
 		if v > highestCount {
 			highestCount = v
 		}
 	}
-	if len(cv) == 1 {
+	if len(cardCounts) == 1 {
 		// nothing to improve
 		return FiveOfAKind
-	} else if len(cv) == 2 && highestCount == 4 {
+	} else if len(cardCounts) == 2 && highestCount == 4 {
 		// Four of a kind: 4+1
-		if cv[hv.JokerCard] > 0 { // remaining card type is a Joker
+		// either one of the card types is a Joker
+		if cardCounts[hv.JokerCard] > 0 {
 			return FiveOfAKind
 		}
 		return FourOfAKind
-	} else if len(cv) == 2 {
+	} else if len(cardCounts) == 2 {
 		// Full House: 3+2
-		if cv[hv.JokerCard] >= 2 { // remaining two cards are a Joker
+		// either one of the card types is a Joker
+		if cardCounts[hv.JokerCard] >= 2 {
 			return FiveOfAKind
 		}
 		return FullHouse
-	} else if len(cv) == 3 && highestCount == 3 {
+	} else if len(cardCounts) == 3 && highestCount == 3 {
 		// Three of a kind: 3+1+1
-		if cv[hv.JokerCard] == 1 || cv[hv.JokerCard] == 3 { // one of the two remaining card types are a Joker
+		// either one of the card types is a Joker
+		if cardCounts[hv.JokerCard] > 0 {
 			return FourOfAKind
 		}
 		return ThreeOfAKind
-	} else if len(cv) == 3 {
+	} else if len(cardCounts) == 3 {
 		// Two pair: 2+2+1
-		if cv[hv.JokerCard] == 2 { // one of the two pairs is of type Joker
+		// one of the two pairs is of type Joker
+		if cardCounts[hv.JokerCard] == 2 {
 			return FourOfAKind
 		}
-		if cv[hv.JokerCard] == 1 { // the single remaining card is a Joker
+		// ..OR the single remaining card is a Joker
+		if cardCounts[hv.JokerCard] == 1 {
 			return FullHouse
 		}
 		return TwoPair
-	} else if len(cv) == 4 {
+	} else if len(cardCounts) == 4 {
 		// One pair: 2+1+1+1
-		// one of the remaining cards OR the pair can be a joker
-		// splitting 2 jokers to make a TwoPair is less powerful than ThreeOfAKind
-		if cv[hv.JokerCard] == 1 || cv[hv.JokerCard] == 2 {
+		// either one of the card types is a Joker
+		// (note: splitting 2 jokers to make a TwoPair is less powerful than ThreeOfAKind)
+		if cardCounts[hv.JokerCard] > 0 {
 			return ThreeOfAKind
 		}
 		return OnePair
 	}
-	if cv[hv.JokerCard] == 1 {
+	// if all cards are different, but there's one Joker, we can make it a Pair
+	if cardCounts[hv.JokerCard] == 1 {
 		return OnePair
 	}
 	return HighCard
