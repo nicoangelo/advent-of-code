bc81c667c18049d967fd106cc9790bb36f8ee28d
veronika <v.schusterbauer@gmx.at>
2024-12-09 12:46:39 +0100

    only run day 6 and 7

--- 2024/main.go
+++ 2024/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/nicoangelo/advent-of-code-2024/day5"
 	"github.com/nicoangelo/advent-of-code-2024/day6"
+	"github.com/nicoangelo/advent-of-code-2024/day7"
 )
 
 func main() {
@@ -10,6 +10,7 @@ func main() {
 	// day2.PrintSolutions()
 	// day3.PrintSolutions()
 	// day4.PrintSolutions()
-	day5.PrintSolutions()
+	// day5.PrintSolutions()
 	day6.PrintSolutions()
+	day7.PrintSolutions()
 }
